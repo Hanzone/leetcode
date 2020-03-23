b0a87bcf3837d83f892db950b231f156b33d6c80
Hanzone <githug@163.com>
2020-03-23 19:48:36 +0800

    217: ContainsDuplicate.

---
+++ src/main/problems/algorithms/p217/ContainsDuplicate.java
@@ -0,0 +1,57 @@
+package problems.algorithms.p217;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import static utils.PrintUtils.print;
+import static utils.PrintUtils.print;
+/**
+ * Given an array of integers, find if the array contains any duplicates.
+ *
+ * Your function should return true if any value appears at least twice in the array,
+ * and it should return false if every element is distinct.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,1]
+ * Output: true
+ * Example 2:
+ *
+ * Input: [1,2,3,4]
+ * Output: false
+ * Example 3:
+ *
+ * Input: [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ */
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null) {
+            return false;
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            } else {
+                set.add(num);
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+
+        print(containsDuplicate.containsDuplicate(new int[]{1, 2, 3, 1}));
+        print(containsDuplicate.containsDuplicate(new int[]{1, 2, 3, 4}));
+        print(containsDuplicate.containsDuplicate(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+    }
+
+
+}
