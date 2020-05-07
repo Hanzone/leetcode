cae818c11ec206d3730f2963f2cf0a485795c5a0
Hanzone <githug@163.com>
2020-05-07 21:49:47 +0800

    62: UniquePaths.

---
+++ src/main/problems/algorithms/p62/UniquePaths.java
@@ -0,0 +1,67 @@
+package problems.algorithms.p62;
+
+import utils.PrintUtils;
+
+/**
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ *
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ *
+ * How many possible unique paths are there?
+ *
+ * https://assets.leetcode.com/uploads/2018/10/22/robot_maze.png
+ *
+ * Example 1:
+ *
+ * Input: m = 3, n = 2
+ * Output: 3
+ * Explanation:
+ * From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down
+ * 2. Right -> Down -> Right
+ * 3. Down -> Right -> Right
+ * Example 2:
+ *
+ * Input: m = 7, n = 3
+ * Output: 28
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= m, n <= 100
+ * It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
+ *
+ */
+public class UniquePaths {
+
+    public int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                // path = top + left
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        UniquePaths uniquePaths = new UniquePaths();
+        int answer1 = uniquePaths.uniquePaths(3, 2);
+        PrintUtils.printAnswerAndExpect(answer1, 3);
+
+        int answer2 = uniquePaths.uniquePaths(7, 3);
+        PrintUtils.printAnswerAndExpect(answer2, 28);
+    }
+
+}
